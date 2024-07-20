cd537139d422860710fca1d88f171767fd7ce574
Melih ismail <melihismail4@gmail.com>
2024-07-20 18:57:40 +0300

    Filter background

--- src/app/models/constants.js
+++ src/app/models/constants.js
@@ -9,13 +9,17 @@ export const infoContainer = document.querySelector(".info-container");
 export const grammarContainer = document.querySelector(".grammar-container");
 
 export const subToggle = document.getElementById("subtitle-toggle");
-export const nextBtn = document.getElementById("nextBtn");
-export const previousBtn = document.getElementById("previousBtn");
-export const startBtn = document.getElementById("start-game-btn");
+
+
 
 export const mainSubtitle = document.getElementById("main-subtitle");
 export const myVideo = document.getElementById("video-shown");
-export const filterBtn = document.getElementById("filter-button");
-export const topicBtn = document.getElementById("topic-button")
+
 export const filterMenu = document.getElementById("filter-menu")
 
+
+export const filterBtn = document.getElementById("filter-button");
+export const topicBtn = document.getElementById("topic-button")
+export const previousBtn = document.getElementById("previousBtn");
+export const startBtn = document.getElementById("start-game-btn");
+export const nextBtn = document.getElementById("nextBtn");
